34e85627c95e92df72423ecdd475e841024029d4
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-03-04 20:49:08 +0400

    I have recorded some idaes for 2.1.1, but I'm not sure am I right or not)

--- Task 2/Task 2.1/Task 2.1/Program.cs
+++ Task 2/Task 2.1/Task 2.1/Program.cs
@@ -6,7 +6,58 @@ namespace Task_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CustomString first = new CustomString();
+            CustomString second = new CustomString();
+            first.Concat("modern", "talking");
+            second.ConvertToString(first.ConvertToCharArray());
+            second.Print();
+            Console.WriteLine(second.SearchSymbol('i')); 
+        }
+    }
+
+    class CustomString
+    {
+        public string value;
+
+        public void Print ()
+        {
+            Console.WriteLine(value);
+        }
+
+        public char[] ConvertToCharArray()
+        {
+            char[] Symbols = value.ToCharArray();
+            return Symbols;
+        }
+
+        public string ConvertToString (char[] SomeSymbols)
+        {
+            value = String.Empty;
+
+            foreach (char item in SomeSymbols)
+            {
+                value += item.ToString();
+            }
+
+            return value;
+        }
+
+        public int SearchSymbol (char Symbol)
+        {
+            int index = value.IndexOf(Symbol);
+            return index;
+        }
+
+        public string Concat (params string[] items)
+        {
+            value = String.Empty;
+
+            foreach (string item in items)
+            {
+                value += item;
+            }
+
+            return value;
         }
     }
 }
