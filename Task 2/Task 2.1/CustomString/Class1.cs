ba38131e5bd59355fd6034aa56d0d2dcb9fc68d9
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-03-12 23:06:45 +0400

    Previous version of the solution was incorrect, I decided to start once again and that is my ideas.

--- Task 2/Task 2.1/CustomString/Class1.cs
+++ Task 2/Task 2.1/CustomString/Class1.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Text;
 
 namespace MyString
 {
     public class CustomString
     {
-        private string _myString = String.Empty;
+        public CustomString(string text)
+        {
+            MyString = text.ToCharArray();
+        }
 
-        public string MyString
+        private char[] _myString;
+
+        public char[] MyString
         {
             get
             {
@@ -18,127 +24,190 @@ namespace MyString
             }
         }
 
-        public void Print()
+        public static CustomString operator + (CustomString str1, CustomString str2)
         {
-            Console.WriteLine(MyString);
-        }
+            string result = String.Empty;
 
-        public char[] ConvertToCharArray()
-        {
-            char[] Symbols = MyString.ToCharArray();
-            return Symbols;
-        }
-
-        public string CreateStringFromChar(params char[] items)
-        {
-            MyString = String.Empty;
-
-            foreach (char item in items)
+            foreach (char item in str1.MyString)
             {
-                MyString += item.ToString();
+                result += item;
             }
 
-            return MyString;
-        }
-
-        public string Concat(params string[] items)
-        {
-            MyString = String.Empty;
-
-            foreach (string item in items)
+            foreach (char item in str2.MyString)
             {
-                MyString += item;
+                result += item;
             }
 
-            return MyString;
+            return new CustomString(result);
         }
 
-        public bool ContainsSymbols(char Symbol)
+        public bool SearchSymbol(char Symbol)
         {
-            int index = MyString.IndexOf(Symbol);
-            if (index == -1)
-                return false;
-            else
-                return true;
-        }
-
-        public int CountSymbol(char Symbol)
-        {
-            int startIndex = 0;
-            int count = -1;
-            int index;
-
-            do
+            bool flag = false;
+            foreach (char item in MyString)
             {
-                index = MyString.IndexOf(Symbol, startIndex + 1);
-                count++;
-                startIndex = index;
+                if (item == Symbol)
+                    flag = true;
             }
-            while (index != -1);
-
-            return count;
+            return flag;
         }
 
-        public int SearchSymbol(char Symbol)
-        {
-            int index = MyString.IndexOf(Symbol);
-            return index;
-        }
+        //public static bool operator == (CustomString str1, CustomString str2)
+        //{
+        //    if (Equals(str1.MyString, str2.MyString) == true)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //        return false;
+        //}
 
-        public int SearchSubstring(string text)
-        {
-            int index = MyString.IndexOf(text);
-            return index;
-        }
+        //public static bool operator != (CustomString str1, CustomString str2)
+        //{
+        //    if (Equals(str1.MyString, str2.MyString) == false)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //        return false;
+        //}
 
-        public string Reverse()
-        {
-            string reverse = string.Empty;
 
-            for (int i = MyString.Length - 1; i > 0; i--)
-            {
-                reverse += MyString[i];
-            }
+        //private string _myString = String.Empty;
 
-            return reverse;
-        }
+        //public string MyString
+        //{
+        //    get
+        //    {
+        //        return _myString;
+        //    }
+        //    set
+        //    {
+        //        _myString = value;
+        //    }
+        //}
 
-        public bool CheckEquality(CustomString obj1, CustomString obj2)
-        {
-            return obj1.MyString.Equals(obj2.MyString);
-        }
+        //public void Print()
+        //{
+        //    Console.WriteLine(MyString);
+        //}
 
-        public int Comparison(CustomString obj1, CustomString obj2)
-        {
-            return obj1.MyString.CompareTo(obj2.MyString);
-        }
+        //public char[] ConvertToCharArray()
+        //{
+        //    char[] Symbols = MyString.ToCharArray();
+        //    return Symbols;
+        //}
 
-        public char this[int index]
-        {
-            get
-            {
-                if (index > MyString.Length - 1 || index < 0)
-                {
-                    return ' ';
-                }
-                else
-                {
-                    return MyString[index];
-                }
-            }
-            set
-            {
-                char[] data = ConvertToCharArray();
-                if (index > MyString.Length - 1 || index < 0)
-                {
-                    Console.WriteLine("Такого индекса не существует! Сначала инициализируйте строку необходимой длины для того, чтобы что-то менять.");
-                }
-                else
-                {
-                    data[index] = value;
-                    CreateStringFromChar(data);
-                }
-            }
-        }
+        //public string CreateStringFromChar(params char[] items)
+        //{
+        //    MyString = String.Empty;
+
+        //    foreach (char item in items)
+        //    {
+        //        MyString += item.ToString();
+        //    }
+
+        //    return MyString;
+        //}
+
+        //public string Concat(params string[] items)
+        //{
+        //    MyString = String.Empty;
+
+        //    foreach (string item in items)
+        //    {
+        //        MyString += item;
+        //    }
+
+        //    return MyString;
+        //}
+
+        //public bool ContainsSymbols(char Symbol)
+        //{
+        //    int index = MyString.IndexOf(Symbol);
+        //    if (index == -1)
+        //        return false;
+        //    else
+        //        return true;
+        //}
+
+        //public int CountSymbol(char Symbol)
+        //{
+        //    int startIndex = 0;
+        //    int count = -1;
+        //    int index;
+
+        //    do
+        //    {
+        //        index = MyString.IndexOf(Symbol, startIndex + 1);
+        //        count++;
+        //        startIndex = index;
+        //    }
+        //    while (index != -1);
+
+        //    return count;
+        //}
+
+        //public int SearchSymbol(char Symbol)
+        //{
+        //    int index = MyString.IndexOf(Symbol);
+        //    return index;
+        //}
+
+        //public int SearchSubstring(string text)
+        //{
+        //    int index = MyString.IndexOf(text);
+        //    return index;
+        //}
+
+        //public string Reverse()
+        //{
+        //    string reverse = string.Empty;
+
+        //    for (int i = MyString.Length - 1; i > 0; i--)
+        //    {
+        //        reverse += MyString[i];
+        //    }
+
+        //    return reverse;
+        //}
+
+        //public bool CheckEquality(CustomString obj1, CustomString obj2)
+        //{
+        //    return obj1.MyString.Equals(obj2.MyString);
+        //}
+
+        //public int Comparison(CustomString obj1, CustomString obj2)
+        //{
+        //    return obj1.MyString.CompareTo(obj2.MyString);
+        //}
+
+        //public char this[int index]
+        //{
+        //    get
+        //    {
+        //        if (index > MyString.Length - 1 || index < 0)
+        //        {
+        //            return ' ';
+        //        }
+        //        else
+        //        {
+        //            return MyString[index];
+        //        }
+        //    }
+        //    set
+        //    {
+        //        char[] data = ConvertToCharArray();
+        //        if (index > MyString.Length - 1 || index < 0)
+        //        {
+        //            Console.WriteLine("Такого индекса не существует! Сначала инициализируйте строку необходимой длины для того, чтобы что-то менять.");
+        //        }
+        //        else
+        //        {
+        //            data[index] = value;
+        //            CreateStringFromChar(data);
+        //        }
+        //    }
+        //}
     }
 }
--- Task 2/Task 2.1/Task 2.1/Program.cs
+++ Task 2/Task 2.1/Task 2.1/Program.cs
@@ -7,11 +7,18 @@ namespace Task_2._1
     {
         static void Main(string[] args)
         {
-            CustomString first = new CustomString();
-            first.Concat("Gooooojood ", "day", "?");
-            Console.WriteLine(first.CountSymbol(first[5]));
-            string reverse = first.Reverse();
-            Console.WriteLine(reverse);
+            CustomString first = new CustomString("Modern");
+            CustomString second = new CustomString("Modern");
+            CustomString third = first + second;
+            bool f = (first == second);
+            Console.WriteLine(f);
+
+            //Console.WriteLine(third.SearchSymbol('i'));
+            //foreach (char item in second.MyString)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
         }
     }
 }
