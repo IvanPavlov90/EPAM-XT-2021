efbd7b6e913cd5d932bc0e6ec4815980cf525612
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-03-23 19:28:44 +0400

    create folder and start logic

---
+++ Task 3/Task 3.1/Task 3.1/Task 3.1/Classes/ListOfPeople.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._1.Classes
+{
+    class ListOfPeople
+    {
+        public ListOfPeople(int number)
+        {
+            QuantityOfPeole = CheckValue(number);
+        }
+
+        public int QuantityOfPeole { get; private set; }
+
+        public static int CheckValue (int value)
+        {
+            if ()
+        }
+    }
+}
---
+++ Task 3/Task 3.1/Task 3.1/Task 3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N:");
+        }
+    }
+}
