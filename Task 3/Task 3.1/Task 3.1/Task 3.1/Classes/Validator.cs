8d3fa3a9e13eb2051c72f166bb43b2c14758ad5c
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-03-23 19:57:19 +0400

    add solution for task 3.1

---
+++ Task 3/Task 3.1/Task 3.1/Task 3.1/Classes/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._1.Classes
+{
+    public static class Validator
+    {
+        public static int InputValue ()
+        {
+            string uservalue = Console.ReadLine();
+            int.TryParse(uservalue, out int value);
+            return value;
+        }
+
+        public static int CheckValue(int value)
+        {
+            while (value <= 0)
+            {
+                Console.WriteLine($"Вы ввели неверное значение. Исправьте, пожалуйста.");
+                string uservalue = Console.ReadLine();
+                int.TryParse(uservalue, out value);
+            }
+            return value;
+        }
+
+        public static void CrossOffPeople (int value, int value2)
+        {
+            if (value < value2) {
+                Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей.");
+            } else
+            {
+                int roundCount = 1;
+                Console.WriteLine($"Сгенерирован круг людей. Начинаем вычеркивать каждого {value2}-го.");
+                while (value >= value2)
+                {
+                    value--;
+                    Console.WriteLine($"Раунд {roundCount}. Вычеркнут человек. Людей осталось: {value}");
+                    roundCount++;
+                }
+                Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей.");
+            }
+        }
+    }
+}
--- Task 3/Task 3.1/Task 3.1/Task 3.1/Program.cs
+++ Task 3/Task 3.1/Task 3.1/Task 3.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Task_3._1.Classes;
 
 namespace Task_3._1
 {
@@ -7,6 +8,10 @@ namespace Task_3._1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите N:");
+            var value = Validator.CheckValue(Validator.InputValue());
+            Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            var value2 = Validator.CheckValue(Validator.InputValue());
+            Validator.CrossOffPeople(value, value2);
         }
     }
 }
