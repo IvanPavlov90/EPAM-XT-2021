941f0e0a647ae6869583f84890a1fc046c51975f
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-04-01 20:52:55 +0400

    cretae new folder add some ideas for methods

---
+++ Task 3/Task 3.3/Task 3.3/Task 3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 2, 4, 5, 7, 8 };
+            SuperArray newSuperArray = new SuperArray(array);
+
+            newSuperArray.MultiplyElem();
+
+            foreach (var item in newSuperArray)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3/Task 3.3/SuperArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace Task_3._3
+{
+    public class SuperArray : IEnumerable
+    {
+        private int[] _myArray { get; set; }
+
+        public SuperArray(int[] array)
+        {
+            _myArray = array;
+        }
+
+        //public int SumElements ()
+        //{
+
+        //}
+
+        //public float AverageElements()
+        //{
+
+        //}
+
+        //public T MostRepeatedElement()
+        //{
+
+        //}
+
+        private readonly Func<int, int> func = new Func<int, int>(Multiply);
+
+        public void MultiplyElem()
+        {
+            for (int i = 0; i < _myArray.Length; i++)
+            {
+                _myArray[i] = func(_myArray[i]);
+            }
+        }
+
+        private static int Multiply(int value)
+        {
+            return value * 2;
+        }
+
+        private IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < _myArray.Length; i++)
+            {
+                yield return _myArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
