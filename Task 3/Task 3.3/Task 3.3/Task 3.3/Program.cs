37ad35a23b424425aea8626fc09965f9b0b8f9ee
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-04-06 22:01:53 +0400

    little fix

--- Task 3/Task 3.3/Task 3.3/Task 3.3/Program.cs
+++ Task 3/Task 3.3/Task 3.3/Task 3.3/Program.cs
@@ -7,17 +7,16 @@ namespace Task_3._3
     {
         static void Main(string[] args)
         {
-            //string text = "Forza Mиlan";
-            //text.CheckLanguage();
+            string text = "Forza Mиlan";
+            text.CheckLanguage();
             byte[] array = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0 };
             Console.WriteLine(array.MostRepeatedByteElement());
-            Console.WriteLine(array.SumByteElements());
-            //int[] array2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0 };
-            //array2.MultiplyIntElem();
-            //foreach (var i in array2)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            int[] array2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0 };
+            array2.MultiplyIntElem();
+            foreach (var i in array2)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
