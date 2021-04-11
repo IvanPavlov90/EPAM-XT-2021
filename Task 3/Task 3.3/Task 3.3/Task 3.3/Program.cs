d5c2f761117926c1b4cca4434a05bb7c1d11187b
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-04-11 10:52:02 +0400

    little fix for 3.3

--- Task 3/Task 3.3/Task 3.3/Task 3.3/Program.cs
+++ Task 3/Task 3.3/Task 3.3/Task 3.3/Program.cs
@@ -7,18 +7,18 @@ namespace Task_3._3
     {
         static void Main(string[] args)
         {
-            //string text = "АаАа";
-            //Console.WriteLine(text.CheckLanguage());
-            //int[] array3 = new int[] { 1, 2, 3, 4, 5};
-            //Console.WriteLine(array3.Average());
+            string text = "АаАа";
+            Console.WriteLine(text.CheckLanguage());
+            int[] array3 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(array3.Average());
             byte[] array = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 2, 2 };
             Console.WriteLine(array.MostRepeatedElement());
-            //int[] array2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0 };
-            //array2.EachElement(item => item - 2);
-            //foreach (var i in array2)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            int[] array2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0 };
+            array2.EachElement(item => item - 2);
+            foreach (var i in array2)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
