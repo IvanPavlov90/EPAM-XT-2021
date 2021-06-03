6ce811a264c1609f56275fe3c32e86f2daf84f04
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-06-03 20:45:50 +0400

    add string check and rename functions 7.1

--- Task 7/Task 7.1/script.js
+++ Task 7/Task 7.1/script.js
@@ -1,30 +1,48 @@
-function charSeparator (text) {
-    let word = [];
-    let repeats = [];
-    textArray = text.split("");
-    for (let i = 0; i < textArray.length; i++) {
-        if (!charComparison(textArray[i])) {
-            if (dataAnalysis(textArray[i], word)) {
-                if (!repeats.includes(textArray[i].toLowerCase())) {
-                    repeats.push(textArray[i].toLowerCase());
+let userText = "null or undefined";
+console.log(changeText(userText));
+
+function changeText (userText) {
+    try {
+        checkText(userText);
+        let word = [];
+        let repeats = [];
+        let textArray = userText.split("");
+        for (let i = 0; i < textArray.length; i++) {
+            if (!comparisingWithSeparators(textArray[i])) {
+                if (findRepetitiveSymbolsInWord(textArray[i], word)) {
+                    if (!repeats.includes(textArray[i].toLowerCase())) {
+                        repeats.push(textArray[i].toLowerCase());
+                    }
                 }
+            } else {
+                word = [];
             }
-        } else {
-            word = [];
-        }
+        } 
+        return deleteDuplicates(textArray, repeats);
+    } catch (e) {
+        alert(e.message);
     }
-    return changeText(textArray, repeats);
+
+}
+
+function checkText (userText) {
+    if (userText === null || userText === undefined) {
+        throw new Error ("You can do nothing with null or undefined")
+    }
 }
 
 /* This function compares symbols from the sentence with separators. 
    Returns true if symbol is separator. */
    
-function charComparison (letter) {
+function comparisingWithSeparators (letter) {
     const separators = [" ", ",", "?", "!", ".", ":", ";"];
     return separators.includes(letter);
 }
 
-function dataAnalysis (letter, word) {
+/* This function analyzes if letter from word repeats or not. 
+   Returns true if symbol repeats inside word.*/
+
+function findRepetitiveSymbolsInWord (letter, word) {
     if (!word.includes(letter.toLowerCase())) {
         word.push(letter.toLowerCase());
     } else {
@@ -32,7 +50,10 @@ function dataAnalysis (letter, word) {
     }
 }
 
-function changeText (textArray, repeats) {
+/* This function search symbols in user's text that are in repeats array.
+   Then it deletes such symbols from user's text. */ 
+
+function deleteDuplicates (textArray, repeats) {
     for (let i = 0; i < textArray.length; i++) {
         if (repeats.includes(textArray[i].toLowerCase())) {
             textArray.splice(i, 1);
@@ -41,5 +62,3 @@ function changeText (textArray, repeats) {
     }
     return textArray.join("");
 }
-
-console.log(charSeparator("Локомотив сегодня выиграл у Спартака со счетом 3-1."));
