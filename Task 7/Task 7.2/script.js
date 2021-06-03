3f2f4c082c40dfcf5267960fb15d9a1a6438e5a9
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-06-03 20:28:20 +0400

    finish task 7.2

--- Task 7/Task 7.2/script.js
+++ Task 7/Task 7.2/script.js
@@ -1,51 +1,95 @@
-function mathCalculator (math) {
-    ariphmeticSigns = getAriphmeticSigns(math);
-    mathArray = getDigits(math);
-    result = getResult (mathArray, ariphmeticSigns)
-    console.log(mathArray, ariphmeticSigns);
-    console.log(result);
+calculateExpression();
+
+function calculateExpression () {
+    let userInput = document.getElementById("userExpression");
+    let output = document.getElementById("result");
+    userInput.addEventListener("blur", (event) => {
+        let result = calculator (event.target.value);
+        if (result === undefined) {
+            output.innerHTML = "";
+        } else {
+            output.innerHTML = "Your result is " + result;
+        }
+    });
 }
 
-function getAriphmeticSigns(math) {
-    signs = [];
-    separators = ["+", "-", "/", "*"];
-    for (let i = 0; i < math.length; i ++) {
-        if (separators.includes(math[i])) {
-            signs.push(math[i]);
+function calculator (userExpression) {
+    try {
+        userExpression = checkExpression(userExpression);
+        let ariphmeticSigns = getAriphmeticSigns(userExpression);
+        let digitsArray = getDigits(userExpression);
+        let result = getResult (digitsArray, ariphmeticSigns);
+        return result;
+    } catch (e) {
+        alert(e.message);
+    }
+}
+
+/* This function checks if format of user's expression is correct or not. */
+
+function checkExpression (expression) {
+    expression = cutRightPartOfExpression(expression.trim());
+    if (expression.startsWith("*") || expression.startsWith("/")) {
+        throw new Error ("Your expression isn't valid.");
+    } else {
+        let regexp = /[0-9]/;
+        let index = expression.search(regexp);
+        if (index == -1 || index > 1) {
+            throw new Error ("Your expression isn't valid.");
+        }
+        return expression;
+    }
+}
+
+/* This method cuts everything that is located after sign "=" */
+
+function cutRightPartOfExpression (expression) {
+    let index = expression.indexOf("=");
+    let result = expression.slice(0, index + 1);
+    return result;
+}
+
+function getAriphmeticSigns(expression) {
+    let signs = [];
+    let separators = ["+", "-", "/", "*"];
+    for (let i = 0; i < expression.length; i ++) {
+        if (separators.includes(expression[i])) {
+            signs.push(expression[i]);
         }
     }
     return signs;
 }
 
-function getDigits (math) {
-    mathArray = math.split(/[\/\*+-]/);
-    return mathArray;
+function getDigits (expression) {
+    let digitsArray = expression.split(/[\/\*+-]/);
+    if (expression.startsWith("+") || expression.startsWith("-")) {
+        digitsArray[0] = 0;
+    }
+    return digitsArray;
 }
 
-function getResult (mathArray, ariphmeticSigns) {
-    result = 0;
-    for (let i = 0; i < mathArray.length; i++) {
+function getResult (digitsArray, ariphmeticSigns) {
+    let result = 0;
+    for (let i = 0; i < digitsArray.length; i++) {
         if (i === 0) {
-            result += parseFloat(mathArray[i]);  
+            result += parseFloat(digitsArray[i]);  
         }
         else {
             switch (ariphmeticSigns[i-1]) {
                 case "+":
-                    result += parseFloat(mathArray[i]);
+                    result += parseFloat(digitsArray[i]);
                     break;
                 case "-":
-                    result -= parseFloat(mathArray[i]);
+                    result -= parseFloat(digitsArray[i]);
                     break;
                 case "*":
-                    result *= parseFloat(mathArray[i]);
+                    result *= parseFloat(digitsArray[i]);
                     break;
                 case "/":
-                    result /= parseFloat(mathArray[i]);
-                     break;
+                    result /= parseFloat(digitsArray[i]);
+                    break;
             }
         }
     }           
     return result.toFixed(2);
 }
-     
-mathCalculator(" 2+ 2    + 2.1    -    6");
