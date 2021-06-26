4ffa8136d83334805f891d14cd6be098b0aa2a2e
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2021-06-26 10:35:45 +0400

    add edit award page

---
+++ Task 8/UsersAndAwards(Framework)/EPAM.AwardsAndUsers.PL.WebPL/Models/DataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPAM.AwardsAndUsers.PL.WebPL.Models
+{
+    public static class DataModel
+    {
+        public static int UserCount { get; set; }
+
+        public static int AwardCount { get; set; }
+    }
+}
